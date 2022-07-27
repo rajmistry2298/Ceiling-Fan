6012015734b133519f697750f829fce5fadc76bb
Raj Mistry <rajmistry2298@gmail.com>
2022-07-26 19:33:44 -0700

    Initial commit

---
+++ src/Main/Fan.java
@@ -0,0 +1,42 @@
+package Main;
+
+/**
+ * Main class Which implements the characteristics of Ceiling Main.Fan.
+ */
+public class Fan {
+    public static final int OFF = 0;
+    //Default speed of Main.Fan is OFF which is 0.
+    private int speed = OFF;
+    //Default fan direction is FORWARD.
+    private String fanDirection = "FORWARD";
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    //This method implements the functions of speed Cord.
+    public void increaseSpeed() {
+        if(this.speed == 3){
+            this.speed = OFF;
+        }else this.speed++;
+    }
+
+    public String getFanDirection() {
+        return fanDirection;
+    }
+
+    //This method implements the functions of Direction Cord.
+    public void reverseDirection() {
+        if(this.fanDirection.equalsIgnoreCase("FORWARD")){
+            this.fanDirection = "REVERSE";
+        }else{
+            this.fanDirection = "FORWARD";
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        return "Main.Fan is working at "+speed+"speed and the direction of Main.Fan is "+ fanDirection + ".";
+    }
+}
---
+++ src/Test/FanTest.java
@@ -0,0 +1,29 @@
+package Test;
+
+
+import Main.Fan;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test Class to test the functions of Ceiling Fan.
+ */
+class FanTest {
+    private Fan fan;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        fan = new Fan();
+    }
+
+    @Test
+    void testIncreaseSpeed(){
+
+    }
+
+    @Test
+    void testReverseDirection(){
+        
+    }
+
+}
